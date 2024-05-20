2aae06ae15e4f4144421415a439cee663067572c
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-20 17:37:31 +0200

    aggiunta funzione genera bombe e funzione game over

--- js/script.js
+++ js/script.js
@@ -29,26 +29,55 @@ playButton.addEventListener('click', function(){
     generaNuovaPartita (griglia, cells, className);
 })
 
-function generaNuovaPartita(container, numeroCelle , livelloScelto){
 
-    container.innerHTML = '';
-
+function generaNuovaPartita(container, numeroCelle, livelloScelto) {
+    container.innerHTML = '';   
+    
+    
     for (let index = 0; index < numeroCelle; index++) {
-        const celleGriglia = document.createElement('article');//creo gli article all'interno della griglia
-    
-        celleGriglia.classList.add('celle', livelloScelto);//aggiungo la class
-    
-        const contenutoCella = document.createElement('span');//creo l'interno della cella con uno span
-        contenutoCella.classList.add('contenuto');// aggiugno una class allo span
-        contenutoCella.append(index + 1); 
-        celleGriglia.appendChild(contenutoCella); // aggiungo lo span all'interno dell'article
-       
-    
-        celleGriglia.addEventListener('click' , function(){
-            celleGriglia.classList.add('active');
-        })
-    
-        griglia.appendChild(celleGriglia);//aggiungo gli article all'interno della griglia
-    
+        const celleGriglia = document.createElement('article');
+        celleGriglia.classList.add('celle', livelloScelto);
+        
+        const contenutoCella = document.createElement('span');
+        contenutoCella.classList.add('contenuto');
+        contenutoCella.append(index + 1);
+        celleGriglia.appendChild(contenutoCella);
+        
+        celleGriglia.addEventListener('click', function() {
+            const numeroCella = index + 1;
+
+            const bombe = generaBombe(16, numeroCelle);
+            let numeriRivelati = 0;
+
+            if (bombe.includes(numeroCella)) {
+                celleGriglia.classList.add('bomba');
+                console.log("Hai cliccato su una bomba! Il gioco è finito.");
+                gameOver(container, numeriRivelati);
+            }  else {
+                celleGriglia.classList.add('active');
+                numeriRivelati++;
+            }
+        });
+        
+        griglia.appendChild(celleGriglia);
     }
+}
+
+function generaBombe(numBombe, max){
+    const bombe = [];
+
+    while (bombe.lenght<numBombe){
+        const numeroRandom = Math.floor(Math.random() * max + 1);
+
+        if (!bombe.includes(numeroRandom)){ // se bombe non include il numero generato
+            bombe.push(numeroRandom); // allora viene aggiunto nell'array
+        }
+
+    }return bombe;
+
+}
+
+
+function gameOver(){
+    alert('hai cliccato su una bomba, il tuo punteggio è' + punteggio);
 }
