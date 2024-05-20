548f94e5d5c2e1b6fa3fd34c44362651804ea23e
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-20 15:31:33 +0200

    aggiunta la select con i vari livelli e la funzione in js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,41 @@ const griglia = document.querySelector('section#griglia');
 
 const playButton = document.querySelector('button#play-button');
 
+const livelloGioco = document.querySelector('select.livello');
+
 playButton.addEventListener('click', function(){
-    generaNuovaPartita (100, griglia);
+    let cells;
+    let className;
+
+    switch (livelloGioco.value){
+        case '0':
+            cells = 100;
+            className = 'facile';
+            break;
+        case '1':
+            cells = 81;
+            className = 'medio';
+            break;
+        case '2':
+            cells = 49;
+            className = 'difficile';
+            break;
+        default:
+            cells = 100;
+            className = 'facile';
+    }
+
+    generaNuovaPartita (griglia, cells, className);
 })
 
-function generaNuovaPartita(numeroCelle , container){
+function generaNuovaPartita(container, numeroCelle , livelloScelto){
 
     container.innerHTML = '';
 
     for (let index = 0; index < numeroCelle; index++) {
         const celleGriglia = document.createElement('article');//creo gli article all'interno della griglia
     
-        celleGriglia.classList.add('celle');//aggiungo la class
+        celleGriglia.classList.add('celle', livelloScelto);//aggiungo la class
     
         const contenutoCella = document.createElement('span');//creo l'interno della cella con uno span
         contenutoCella.classList.add('contenuto');// aggiugno una class allo span
